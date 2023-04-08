3a8f494db3d339abee2581ebd71e2dae70c487e7
Jesus Mancilla <spaceairmac@Jesuss-MacBook-Air.local>
2023-04-08 15:52:39 -0700

    now is a chatbot!

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,15 +1,15 @@
 const { Configuration, OpenAIApi } = require("openai");
-const axios = require('axios');
+const axios = require("axios");
 
-const apiKey = process.env.OPENAI_API_KEY
-console.log(apiKey)
+const apiKey = process.env.OPENAI_API_KEY;
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  console.log(apiKey)
+  const question = req.body.question || "";
+  const conversationHistory = req.body.conversationHistory || [];
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -19,7 +19,6 @@ export default async function (req, res) {
     return;
   }
 
-  const question = req.body.question || '';
   if (question.trim().length === 0) {
     res.status(400).json({
       error: {
@@ -31,25 +30,24 @@ export default async function (req, res) {
 
   try {
     const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
+      "https://api.openai.com/v1/chat/completions",
       {
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {role: "system", content: "You are a member of the Silicon Valley Reformed Baptist Church. You believe the Bible has the ultimate authority to determine what people believe and do. Many affirm this Bible and arrive at different conclusions about its teachings. In light of this reality, we have adopted the 1689 London Baptist Confession of Faith that expresses our understanding of the Bible’s vision for the church to promote clarity and transparency at Silicon Valley Reformed Baptist Church."},
-          {role: "user", content: question}
-        ],
+        model: "gpt-3.5-turbo",
+        messages: [...conversationHistory, { role: "user", content: question }],
       },
       {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
         },
-      },
+      }
     );
-    const result = response.data.choices[0].message;
-    console.log(result);
-    res.status(200).json({ result });
-  } catch(error) {
+    const assistant = response.data.choices[0].message;
+    const newConversationHistory = [...conversationHistory, { role: "user", content: question }, assistant];
+    res.status(200).json({ newConversationHistory });
+    console.log("newConversationHistory", newConversationHistory);
+  } catch (error) {
+
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,35 @@ export default function Home() {
   const [questionInput, setQuestionInput] = useState("");
   const [result, setResult] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [conversationStarted, setConversationStarted] = useState(false);
+  const [conversationHistory, setConversationHistory] = useState([{ role: "system", content: "You are a member of the Silicon Valley Reformed Baptist Church. You believe the Bible has the ultimate authority to determine what people believe and do. Many affirm this Bible and arrive at different conclusions about its teachings. In light of this reality, we have adopted the 1689 London Baptist Confession of Faith that expresses our understanding of the Bible’s vision for the church to promote clarity and transparency at Silicon Valley Reformed Baptist Church." }]);
 
   async function onSubmit(event) {
     event.preventDefault();
     setIsLoading(true); // Set loading state to true
-    event.preventDefault();
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ question: questionInput }),
+        body: JSON.stringify({ question: questionInput, conversationHistory }),
       });
-  
+
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-  
-      setResult(data.result.content); // Access the 'content' key of the returned object
+
+      // Update conversationHistory
+      const newConversationHistory = [
+        ...conversationHistory,
+        { role: "user", content: questionInput },
+        { role: "assistant", content: data.newConversationHistory[data.newConversationHistory.length - 1].content },
+      ];
+      setConversationHistory(newConversationHistory);
       setQuestionInput("");
+      setConversationStarted(true);
     } catch (error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -44,21 +52,34 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/calvinist_parrot.gif" className={styles.icon} alt="Calvinist Parrot" />
-        <h3>What theological questions do you have?</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="question"
-            placeholder="ask a question"
-            value={questionInput}
-            onChange={(e) => setQuestionInput(e.target.value)}
-          />
-          <input type="submit" value="Ask" />
-          {isLoading && <span>Loading...</span>}
+        <div className={styles.fixedContent}>
+          <img src="/calvinist_parrot.gif" className={styles.icon} alt="Calvinist Parrot" />
+          <h3>What theological questions do you have?</h3>
+        </div>
+        <div className={styles.scrollableContent}>
+            {conversationStarted &&
+              conversationHistory.map((message, index) => (
+                <p key={index}>
+                  <strong>{message.role === "user" ? "Your question" : "Calvinist Parrot"}:</strong> {message.content}
+                </p>
+              ))}
+        </div>
+        <div className={styles.formContainer}>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="question"
+              placeholder="ask a question"
+              value={questionInput}
+              onChange={(e) => setQuestionInput(e.target.value)}
+            />
+            <input type="submit" value="Ask" />
+            {isLoading && <span>Loading...</span>}
           </form>
-        <div className={styles.result}>{result}</div>
+        </div>
       </main>
+
+
     </div>
   );
 }
