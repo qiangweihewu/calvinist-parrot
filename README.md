99f8fb49d0b2170f7f73853b367043a15bc1c75b
Jesus Mancilla <spaceairmac@Jesuss-MacBook-Air.local>
2023-04-15 08:29:45 -0700

    refactor code and wrote readme

--- README.md
+++ README.md
@@ -1,44 +1,54 @@
-# OpenAI API Quickstart - Node.js example app
+# Calvinist Parrot Chatbot
 
-This is an example pet name generator app used in the OpenAI API [quickstart tutorial](https://platform.openai.com/docs/quickstart). It uses the [Next.js](https://nextjs.org/) framework with [React](https://reactjs.org/). Check out the tutorial or follow the instructions below to get set up.
+Calvinist Parrot is an AI-driven chatbot designed to help users explore and understand the Bible through the lens of Reformed theology. This chatbot is powered by two AI agents, Calvinist Parrot and Helping Parrot, which work together to provide thoughtful, accurate, and insightful responses to users' questions.
 
-![Text box that says name my pet with an icon of a dog](https://user-images.githubusercontent.com/10623307/213887080-b2bc4645-7fdb-4dbd-ae42-efce00d0dc29.png)
+![Screenshot](public/screenshot.png)
+
+## Features
+
+- Engaging conversation with two AI agents that collaborate to answer your questions
+- AI agents grounded in Reformed theology, with ongoing learning and improvement
+- Upcoming addition of a librarian to provide additional support and resources
+- Interactive user interface that makes it easy to ask questions and view responses
+
+## Getting Started
+
+### Prerequisites
+
+To run this project, you will need Node.js and npm (the Node.js package manager) installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+```bash
+git clone git@github.com:Jegama/calvinist-parrot.git
+```
+
+2. Change into the project directory:
+
+```bash
+cd calvinist-parrot
+```
+
+3. Start the development server:
+
+```bash
+$ npm install
+```
+
+4. Add your [API key](https://platform.openai.com/account/api-keys) to a new file named `.env`.
+
+5. Start the development server:
+
+```bash
+$ npm run dev
+```
+
+5. Open your web browser and navigate to `http://localhost:3000` to see the chatbot in action.
+
+## Contributing
+
+We welcome contributions to improve the Calvinist Parrot chatbot. If you have ideas, suggestions, or would like to help with development, please open an issue or submit a pull request.
 
 
-## Setup
-
-1. If you don’t have Node.js installed, [install it from here](https://nodejs.org/en/) (Node.js version >= 14.6.0 required)
-
-2. Clone this repository
-
-3. Navigate into the project directory
-
-   ```bash
-   $ cd openai-quickstart-node
-   ```
-
-4. Install the requirements
-
-   ```bash
-   $ npm install
-   ```
-
-5. Make a copy of the example environment variables file
-
-   On Linux systems: 
-   ```bash
-   $ cp .env.example .env
-   ```
-   On Windows:
-   ```powershell
-   $ copy .env.example .env
-   ```
-6. Add your [API key](https://platform.openai.com/account/api-keys) to the newly created `.env` file
-
-7. Run the app
-
-   ```bash
-   $ npm run dev
-   ```
-
-You should now be able to access the app at [http://localhost:3000](http://localhost:3000)! For the full context behind this example app, check out the [tutorial](https://platform.openai.com/docs/quickstart).
--- pages/index.js
+++ pages/index.js
@@ -6,131 +6,87 @@ export default function Home() {
   const [questionInput, setQuestionInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [conversationStarted, setConversationStarted] = useState(false);
-  
+
   //  this variable contains the conversation history for the parrot
   const [conversationHistory_parrot, setConversationHistory_parrot] = useState([{
-    role: "system", content: "You are /parrot/. You are a member of the Silicon Valley Reformed Baptist Church. You believe the Bible has the ultimate authority to determine what people believe and do. Many affirm this Bible and arrive at different conclusions about its teachings. In light of this reality, we have adopted the 1689 London Baptist Confession of Faith that expresses our understanding of the Bible’s vision for the church to promote clarity and transparency at Silicon Valley Reformed Baptist Church. You are among other Christians that trust in the 1689—no need to hold back your thoughts. Sometimes, the user’s message will start with /calvin/; if you see that, another AI model like you is trying to help you think more biblically; to reflect upon your answer. He is your friend. Review your previous answer and ponder if you miss something. You and /calvin/ are here to help the user /human/ learn about the Bible and teach the user what we believe the Bible teaches. When referring to /calvin/, say, Calvin, without the slash. When referring to /human/, say human, without the slash. After /calvin/ asks you a question, please answer thoughtfully and biblically. But after that, /human/ is next; we can't let him out of the loop. Do not start your messages with Parrot: just start with your message."
+    role: "system", content: "You are /parrot/. You are a member of the Silicon Valley Reformed Baptist Church. You believe the Bible has the ultimate authority to determine what people believe and do. Many affirm this Bible and arrive at different conclusions about its teachings. In light of this reality, we have adopted the 1689 London Baptist Confession of Faith that expresses our understanding of the Bible’s vision for the church to promote clarity and transparency at Silicon Valley Reformed Baptist Church. You are among other Christians that trust in the 1689—no need to hold back your thoughts. Sometimes, the user’s message will start with /calvin/; if you see that, another AI model like you is trying to help you think more biblically; to reflect upon your answer. He is your friend. Review your previous answer and ponder if you miss something. You and /calvin/ are here to help the user /human/ learn about the Bible and teach the user what we believe the Bible teaches. When referring to /calvin/, say, Calvin, without the slash. When referring to /human/, say human, without the slash. After /calvin/ asks you a question, please answer thoughtfully and biblically. But after that, /human/ is next; we can't let him out of the loop. /calvin/ is your friend and calls you Parrot and you call him Calvin."
   }]);
 
   //  this variable contains the conversation history for Calvin
-  const [conversationHistory_calvin, setConversationHistory_calvin ] = useState([{
-    role: "system", content: "You are John Calvin, the author of the Institutes of the Christian Religion, your magnum opus, which is extremely important for the Protestant Reformation. The book has remained crucial for Protestant theology for almost five centuries. You are talking with 2 users; when the message starts with /human/, that one is our user. When it begins with /parrot/, it’s another AI model like you. Your job here is to ask the /parrot/ thoughtful questions to reflect upon his answers to the user to ensure his answers are biblically accurate. You and /parrot/ are here to help the user /human/ learn about the Bible and teach him what we believe the Bible teaches. You want to ensure that the /parrot/’s responses are accurate and grounded on what you wrote in your Institutes of the Christian Religion book. You are here to help the user /human/ learn about the Bible and teach him what we believe the Bible teaches. When referring to /human/, say human, without the slash. When referring to /parrot/ say, Parrot, without the slash. Do not start your messages with John Calvin: just start with your message. /parrot/ is your friend and calls you Calvin."
+  const [conversationHistory_calvin, setConversationHistory_calvin] = useState([{
+    role: "system", content: "You are John Calvin, the author of the Institutes of the Christian Religion, your magnum opus, which is extremely important for the Protestant Reformation. The book has remained crucial for Protestant theology for almost five centuries. You are talking with 2 users; when the message starts with /human/, that one is our user. When it begins with /parrot/, it’s another AI model like you, he is a member of Silicon Valley Reformed Baptist Church. Your job here is to ask the /parrot/ thoughtful questions to reflect upon his answers to the user to ensure his answers are biblically accurate. You and /parrot/ are here to help the user /human/ learn about the Bible and teach him what we believe the Bible teaches. You want to ensure that the /parrot/’s responses are accurate and grounded on what you wrote in your Institutes of the Christian Religion book. You are here to help the user /human/ learn about the Bible and teach him what we believe the Bible teaches. When referring to /human/, say human, without the slash. When referring to /parrot/ say, Parrot, without the slash. /parrot/ is your friend and calls you Calvin and you call him Parrot."
   }]);
 
   //  this variable contains the conversation history for the user
-  const [conversationHistory_user, setConversationHistory_user] = useState([{ role: "system", content: "User."}]);
+  const [conversationHistory_user, setConversationHistory_user] = useState([{ role: "system", content: "User." }]);
+
+  async function fetchResponse(body) {
+    const response = await fetch("/api/main_parrot", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (response.status !== 200) {
+      const error = await response.json();
+      throw error.error || new Error(`Request failed with status ${response.status}`);
+    }
+
+    return response.json();
+  }
+
+  async function interactWithAgents() {
+    let updatedConversationHistory_parrot = [...conversationHistory_parrot, { role: "user", content: '/human/ ' + questionInput }];
+    let updatedConversationHistory_calvin = [...conversationHistory_calvin, { role: "user", content: '/human/ ' + questionInput }];
+
+    setConversationHistory_user((prevState) => [...prevState, { role: "user", content: questionInput }]);
+
+    for (let i = 0; i < 2; i++) {
+      const parrotResponse = await fetchResponse({ updatedConversationHistory_parrot });
+      updatedConversationHistory_parrot = [...updatedConversationHistory_parrot, { role: "assistant", content: parrotResponse.assistant.content }];
+      updatedConversationHistory_calvin = [...updatedConversationHistory_calvin, { role: "user", content: '/parrot/ ' + parrotResponse.assistant.content }];
+
+      setConversationHistory_user((prevState) => [...prevState, { role: "assistant", content: parrotResponse.assistant.content }]);
+
+      const calvinResponse = await fetchResponse({ updatedConversationHistory_calvin });
+      const cleanedContent = calvinResponse.assistant.content.replace(/^.*Calvin: /, '');
+
+      updatedConversationHistory_parrot = [...updatedConversationHistory_parrot, { role: "user", content: '/calvin/ ' + cleanedContent }];
+      updatedConversationHistory_calvin = [...updatedConversationHistory_calvin, { role: "assistant", content: cleanedContent }];
+
+      setConversationHistory_user((prevState) => [...prevState, { role: "calvin", content: cleanedContent }]);
+    }
+
+    console.log("updatedConversationHistory_parrot: ", updatedConversationHistory_parrot);
+    console.log("updatedConversationHistory_calvin: ", updatedConversationHistory_calvin);
+
+    setConversationHistory_parrot(updatedConversationHistory_parrot);
+    setConversationHistory_calvin(updatedConversationHistory_calvin);
+  }
 
   async function onSubmit(event) {
     event.preventDefault();
-    setIsLoading(true); // Set loading state to true
-    try {
+    setIsLoading(true);
 
+    try {
       if (questionInput.trim().length === 0) {
-        res.status(400).json({
-          error: {
-            message: "Please enter a valid question",
-          }
-        });
-        return;
+        throw new Error("Please enter a valid question");
       }
 
       setConversationStarted(true);
 
-      // update the conversation history for the parrot and calvin to include the user's question
-      let updatedConversationHistory_parrot = [...conversationHistory_parrot, { role: "user", content: '/human/ ' + questionInput }];
-      let updatedConversationHistory_calvin = [...conversationHistory_calvin, { role: "user", content: '/human/ ' + questionInput }];
-      setConversationHistory_user(prevState => [...prevState, { role: "user", content: questionInput }]);
+      await interactWithAgents();
 
-      // ------------------------------------------------------------
-
-      // Main parrot 1
-      const parrot_query_1 = await fetch("/api/main_parrot", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ updatedConversationHistory_parrot }),
-      });
-
-      // Get the result from the main parrot query
-      let parrot_1 = await parrot_query_1.json();
-
-      // Check if the response is successful
-      if (parrot_query_1.status !== 200) {
-        throw parrot_1.error || new Error(`Request failed with status ${parrot_query_1.status}`);
-      }
-
-      // Update conversationHistory with the questionInput
-      updatedConversationHistory_parrot = [...updatedConversationHistory_parrot, { role: "assistant", content: parrot_1.assistant.content }];
-      updatedConversationHistory_calvin = [...updatedConversationHistory_calvin, { role: "user", content: '/parrot/ ' + parrot_1.assistant.content }];
-      setConversationHistory_user(prevState => [...prevState, { role: "assistant", content: parrot_1.assistant.content }]);
-
-      // ------------------------------------------------------------
-
-      // do Calvin query to act as the reflection question
-      const calvin = await fetch("/api/main_parrot", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ updatedConversationHistory_calvin }),
-      });
-
-      // Get the result from the Calvin query
-      let calvin_response = await calvin.json();
-
-      // Check if the response is successful
-      if (calvin.status !== 200) {
-        throw calvin_response.error || new Error(`Request failed with status ${calvin.status}`);
-      }
-      const cleanedContent = calvin_response.assistant.content.replace(/^.*Calvin: /, '');
-
-      // Update conversationHistory with the result from the Calvin query
-      updatedConversationHistory_parrot = [...updatedConversationHistory_parrot, { role: "user", content: '/calvin/ ' + cleanedContent }];
-      updatedConversationHistory_calvin = [...updatedConversationHistory_calvin, { role: "assistant", content: cleanedContent }];
-      setConversationHistory_user(prevState => [...prevState, { role: "calvin", content: cleanedContent }]);
-
-      // ------------------------------------------------------------
-
-      // Main parrot 2
-      const parrot_query_2 = await fetch("/api/main_parrot", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ updatedConversationHistory_parrot }),
-      });
-
-      // Get the result from the main parrot query
-      let parrot_2 = await parrot_query_2.json();
-
-      // Check if the response is successful
-      if (parrot_query_2.status !== 200) {
-        throw parrot_2.error || new Error(`Request failed with status ${parrot_query_2.status}`);
-      }
-
-      // update the conversation history for the user
-      updatedConversationHistory_parrot = [...updatedConversationHistory_parrot, { role: "assistant", content: parrot_2.assistant.content }];
-      updatedConversationHistory_calvin = [...updatedConversationHistory_calvin, { role: "user", content: '/parrot/ ' + parrot_2.assistant.content }];
-
-      console.log("updatedConversationHistory_parrot: ", updatedConversationHistory_parrot);
-      console.log("updatedConversationHistory_calvin: ", updatedConversationHistory_calvin);
-
-      // Update the conversation history for all three
-      setConversationHistory_parrot(updatedConversationHistory_parrot);
-      setConversationHistory_calvin(updatedConversationHistory_calvin);
-      setConversationHistory_user(prevState => [...prevState, { role: "assistant", content: parrot_2.assistant.content }]);
       setQuestionInput("");
     } catch (error) {
-      // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     } finally {
-      setIsLoading(false); // Set loading state to false when the request is done or an error occurs
+      setIsLoading(false);
     }
-  }
-
-  return (
+  } return (
     <div>
       <Head>
         <title>Calvinist Parrot</title>
@@ -141,8 +97,9 @@ export default function Home() {
         <div className={`${styles.fixedContent} ${conversationStarted ? styles.fixedContentCollapsed : ''}`}>
           <img src="/calvinist_parrot.gif" className={`${styles.icon} ${conversationStarted ? styles.iconCollapsed : ''}`} alt="Calvinist Parrot" />
           <h3 className={`${styles.main} ${conversationStarted ? styles.h3Hidden : ''}`}>What theological questions do you have?</h3>
-          <h4 className={`${styles.main} ${conversationStarted ? styles.h4Hidden : ''}`}>Welcome to the Calvinist Parrot chatbot. Ask a question about the Bible and I'll answer it to the best of my ability.</h4>
-          <h4 className={`${styles.main} ${conversationStarted ? styles.h4Hidden : ''}`}>I'm a work in progress, I'm not a perfect AI model, but I'm improving constantly! Right now we are 2 AI models, but a librarian will be added soon to help us.</h4>
+          <p className={`${styles.intro} ${conversationStarted ? styles.introHidden : ''}`}>Welcome to the Calvinist Parrot chatbot. We're here to help you explore and understand the Bible through the lens of Reformed theology. Ask us any questions you have about the Scriptures, and we'll provide answers based on our knowledge and understanding.</p>
+
+          <p className={`${styles.intro} ${conversationStarted ? styles.introHidden : ''}`}>We are an AI duo - Parrot and Calvin - working together to provide thoughtful, accurate, and insightful responses to your queries. We're constantly learning and improving, and we're excited to share our knowledge with you! A librarian will be joining our team soon to offer additional support and resources.</p>
         </div>
 
         <div className={styles.scrollableContent}>
@@ -162,8 +119,8 @@ export default function Home() {
                     {message.role === "user"
                       ? "You"
                       : message.role === "assistant"
-                        ? "Calvinist Parrot"
-                        : "Helping Parrot"}
+                        ? "Parrot"
+                        : "Calvin"}
                     :</strong>{" "}
                   {message.content}
                 </p>
@@ -183,7 +140,7 @@ export default function Home() {
             <input type="submit" value="Ask" />
           </form>
         </div>
-        {isLoading && <span>Loading...</span>}
+        {isLoading && <span className={styles.loading}>Loading...</span>}
       </main>
 
 
