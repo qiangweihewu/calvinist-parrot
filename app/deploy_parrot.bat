883a1ed4eed8da6fb8b3c145ef6c83219d36ebde
Jesus Mancilla <jegama70@gmail.com>
2023-10-08 16:46:14 -0700

    reduce costs

--- app/deploy_parrot.bat
+++ app/deploy_parrot.bat
@@ -1,20 +1,20 @@
-# build docker image
+@REM # build docker image
 docker build -t streamlit_parrot .
 
-# run docker image locally
-docker run -p 8080:8080 streamlit_parrot
+@REM # run docker image locally
+@REM docker run -p 8080:8080 streamlit_parrot
 
-# push docker image to gcloud
-gcloud auth configure-docker us-west1-docker.pkg.dev
+@REM # push docker image to gcloud
+@REM gcloud auth configure-docker us-west1-docker.pkg.dev
 
-# tag docker image
+@REM # tag docker image
 docker tag streamlit_parrot us-west1-docker.pkg.dev/calvinist-parrot/streamlit-parrot/streamlit-parrot
 
-# create repository
-gcloud artifacts repositories create streamlit-parrot --repository-format=docker --location=us-west1 --description="streamlit-parrot"
+@REM # create repository
+@REM gcloud artifacts repositories create streamlit-parrot --repository-format=docker --location=us-west1 --description="streamlit-parrot"
 
-# push docker image
+@REM # push docker image
 docker push us-west1-docker.pkg.dev/calvinist-parrot/streamlit-parrot/streamlit-parrot
 
-# deploy docker image to cloud run
+@REM # deploy docker image to cloud run
 gcloud run deploy streamlit-parrot --image us-west1-docker.pkg.dev/calvinist-parrot/streamlit-parrot/streamlit-parrot --region us-west1 --platform managed --allow-unauthenticated --port 8080 --memory 512Mi --cpu 1 --timeout 300 --max-instances 4 --min-instances 1
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from llama_index import ServiceContext
 from pydantic import BaseModel
-from llama_index.llms import ChatMessage, MessageRole
-from llama_index.prompts import ChatPromptTemplate
+# from llama_index.llms import ChatMessage, MessageRole
+# from llama_index.prompts import ChatPromptTemplate
 from llama_index.indices.loading import load_index_from_storage
 
 load_dotenv('app/.env')
@@ -29,48 +29,48 @@ service_context = ServiceContext.from_defaults(
 
 llama_index.set_global_service_context(service_context)
 
-# Text QA Prompt
-chat_text_qa_msgs = [
-    ChatMessage(
-        role=MessageRole.SYSTEM,
-        content="Always answer the question. If the context isn't helpful, try again.",
-    ),
-    ChatMessage(
-        role=MessageRole.USER,
-        content=(
-            "Context information is below.\n"
-            "---------------------\n"
-            "{context_str}\n"
-            "---------------------\n"
-            "Given the context information and not prior knowledge, "
-            "answer the question: {query_str}\n"
-        ),
-    ),
-]
-text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
+# # Text QA Prompt
+# chat_text_qa_msgs = [
+#     ChatMessage(
+#         role=MessageRole.SYSTEM,
+#         content="Always answer the question. If the context isn't helpful, try again.",
+#     ),
+#     ChatMessage(
+#         role=MessageRole.USER,
+#         content=(
+#             "Context information is below.\n"
+#             "---------------------\n"
+#             "{context_str}\n"
+#             "---------------------\n"
+#             "Given the context information and not prior knowledge, "
+#             "answer the question: {query_str}\n"
+#         ),
+#     ),
+# ]
+# text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
 
-# Refine Prompt
-chat_refine_msgs = [
-    ChatMessage(
-        role=MessageRole.SYSTEM,
-        content="Always answer the question. If the context isn't helpful, try again.",
-    ),
-    ChatMessage(
-        role=MessageRole.USER,
-        content=(
-            "We have the opportunity to refine the original answer "
-            "(only if needed) with some more context below.\n"
-            "------------\n"
-            "{context_msg}\n"
-            "------------\n"
-            "Given the new context, refine the original answer to better "
-            "answer the question: {query_str}. "
-            "If the context isn't useful, output the original answer again.\n"
-            "Original Answer: {existing_answer}"
-        ),
-    ),
-]
-refine_template = ChatPromptTemplate(chat_refine_msgs)
+# # Refine Prompt
+# chat_refine_msgs = [
+#     ChatMessage(
+#         role=MessageRole.SYSTEM,
+#         content="Always answer the question. If the context isn't helpful, try again.",
+#     ),
+#     ChatMessage(
+#         role=MessageRole.USER,
+#         content=(
+#             "We have the opportunity to refine the original answer "
+#             "(only if needed) with some more context below.\n"
+#             "------------\n"
+#             "{context_msg}\n"
+#             "------------\n"
+#             "Given the new context, refine the original answer to better "
+#             "answer the question: {query_str}. "
+#             "If the context isn't useful, output the original answer again.\n"
+#             "Original Answer: {existing_answer}"
+#         ),
+#     ),
+# ]
+# refine_template = ChatPromptTemplate(chat_refine_msgs)
 
 with open('precomputed_results/ccel_storage_context.pkl', 'rb') as f:
     ccel_storage_context = pickle.load(f)
@@ -78,8 +78,7 @@ with open('precomputed_results/ccel_storage_context.pkl', 'rb') as f:
 ccel_index = load_index_from_storage(ccel_storage_context)
 ccel_query_engine = ccel_index.as_query_engine(
     similarity_top_k=10,
-    text_qa_template=text_qa_template, 
-    refine_template=refine_template
+    response_mode="refine"
 )
 
 class Question(BaseModel):
